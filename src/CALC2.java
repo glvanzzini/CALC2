26fdd74e567b5c363d82b4735be915650176b869
Giampiero <glvanzzini@gmail.com>
2016-10-17 23:53:43 -0500

    CALC2 Final version.

--- src/CALC2.java
+++ src/CALC2.java
@@ -1,7 +1,8 @@
 import java.util.*;
 
 /**
- * Created by Giampiero on 9/19/2016.
+ * Created by Giampiero Vanzzini on 10/14/2016.
+ * Program: CALC 2
  */
 public class CALC2 {
 
@@ -9,18 +10,18 @@ public class CALC2 {
     private static Map<String, Double> lstVars = new HashMap<>();
     private static Scanner in;
     public static void main(String[] args){
-        System.out.println("You must put a space between all \"sqrt\" and values or variables.");
+       // System.out.println("You must put a space between all \"sqrt\" and values or variables.");
         System.out.println("$>READY FOR INPUT");
 
         String command = "";
-        while (true){
+        while (true){ //this loops keeps running until you input "stop"
             System.out.print("$>");
-            in = new Scanner(System.in);
+            in = new Scanner(System.in); //takes command
             command = in.nextLine();
             if (command.contains("stop")){
                 break;
             }
-            ProcessCommand(command);
+            ProcessCommand(command); //starts processing command
             in.reset();
         }
         in.close();
@@ -29,38 +30,38 @@ public class CALC2 {
     }
 
     private static void ProcessCommand(String command) {
-        if(command.isEmpty()){
+        if(command.isEmpty()){ //dont waste time on empty commands
             //System.out.println();
             return;
         }
         String s = "";
-        if(command.contains("//")) {
+        if(command.contains("//")) { //handles comments
             s = command.substring(0, command.indexOf("//"));
         }
-        System.out.println(s);
+
         String temp;
+        String newStr = command.replaceAll("=", " ").trim();
         String firstCommand = "";
         String restOfCommand = "";
-        in = new Scanner(command).useDelimiter(" ");
+        in = new Scanner(newStr).useDelimiter(" ");
         firstCommand = in.next().trim();
         if(firstCommand.length() > 0){
             temp = in.nextLine().trim();
             if(temp.contains("//")){
-                // String s = temp.substring(0, temp.indexOf("//"));
                 String[] splitter = temp.split("//");
                 restOfCommand = splitter[0];
             } else {
                 restOfCommand = temp;
             }
             in.reset();
-            switch(firstCommand){
+            switch(firstCommand){ //decides what to do with a command
                 case "load": Load(restOfCommand);
                     break;
-                case "mem": Mem(restOfCommand);
+                case "mem": Mem(restOfCommand); //stores variable
                     break;
-                case "print": Print(restOfCommand);
+                case "print": Print(restOfCommand); //print variable
                     break;
-                default: ParseMathString(firstCommand, restOfCommand);
+                default: ParseMathString(firstCommand, restOfCommand); //evaluates math string
             }
         }
 
@@ -69,17 +70,17 @@ public class CALC2 {
     private static void ParseMathString(String firstCommand, String restOfCommand) {
         in.reset();
 
-        String edits = restOfCommand.replaceAll("sqrt ", "&").trim();
-        String newStr = edits.replaceAll("=", "").trim();
-        String newStr2 = newStr.replaceAll(" ", "").trim();
+        //String edits = restOfCommand.replaceAll("sqrt ", "&").trim();
 
-        String strParsed = newStr2;
+        String newStr2 = restOfCommand.replaceAll(" ", "").trim(); //remove white space
+
+        String strParsed = newStr2; //it works so I'm leaving it here.
 
         //in = new Scanner(restOfCommand).useDelimiter("-|+|*|/|^|sqrt");
-        String[] tempNums = strParsed.split("\\W");
+        String[] tempNums = strParsed.split("\\W"); //for getting variables in the string
 
         Double[] numbers = new Double[tempNums.length];
-        for(int i = 0; i < numbers.length; i++){
+        for(int i = 0; i < numbers.length; i++){ //evaluate variables in the string before evaluating the string.
             if (isNumeric(tempNums[i])){
                 numbers[i] = Double.parseDouble(tempNums[i]);
             }else{
@@ -91,19 +92,22 @@ public class CALC2 {
 
             }
         }
-        String moreStr = AddWhiteSpace(strParsed);
-        String sqrtGone = moreStr.replaceAll(" 1 ", "1.0");
-        String parsed = sqrtGone.replaceAll("& 1.0 ", "1.0");
-        String finalParse = RemoveSqrt(parsed);
-        eval doThis = new eval();
-        System.out.print(doThis.performMath(finalParse));
-        if(lstVars.containsKey(firstCommand)){
-            lstVars.put(firstCommand, Calculate(finalParse));
+        String moreStr = AddWhiteSpace(strParsed); //put some white space back in there
+        //deprecated:
+//        System.out.println(moreStr);
+//        System.out.println();
+        //String sqrtGone = moreStr.replaceAll(" 1 ", "1.0");
+        //String parsed = sqrtGone.replaceAll("& 1.0 ", "1.0");
+        //String finalParse = RemoveSqrt(parsed);
+        //eval doThis = new eval();
+        //System.out.print(doThis.performMath(finalParse));
+        if(lstVars.containsKey(firstCommand)){ //if the variable exists, do the math.
+            lstVars.put(firstCommand, performMath(moreStr));
         }
 
     }
 
-    private static String AddWhiteSpace(String restOfCommand) {
+    private static String AddWhiteSpace(String restOfCommand) { //adds white space to a math string.
         if(restOfCommand.contains("*")){
             restOfCommand = restOfCommand.replaceAll("\\*", " * ");
         }
@@ -119,13 +123,14 @@ public class CALC2 {
         if(restOfCommand.contains("^")){
             restOfCommand = restOfCommand.replaceAll("\\^", " ^ ");
         }
-        if(restOfCommand.contains("&")){
-            restOfCommand = restOfCommand.replaceAll("\\&", "& ");
-        }
+//        if(restOfCommand.contains("&")){
+//            restOfCommand = restOfCommand.replaceAll("", "& ");
+//        }
         return restOfCommand.trim();
     }
 
-    private static String RemoveSqrt(String strMath){
+/*    deprecated:
+private static String RemoveSqrt(String strMath){
         String current = "";
         String returnStr = "";
         in.reset();
@@ -174,6 +179,87 @@ public class CALC2 {
 
         }
         return total;
+    }*/
+
+//this does math stuff.
+    public static double performMath(final String str) { //this does all the math evaluation work
+        return new Object() {
+            int pos = -1, ch;
+
+            void nextChar() {
+                ch = (++pos < str.length()) ? str.charAt(pos) : -1;
+            }
+
+            boolean eat(int charToEat) {
+                while (ch == ' ') nextChar();
+                if (ch == charToEat) {
+                    nextChar();
+                    return true;
+                }
+                return false;
+            }
+
+            double parse() {
+                nextChar();
+                double x = parseExpression();
+                if (pos < str.length()) throw new RuntimeException("Unexpected: " + (char)ch);
+                return x;
+            }
+
+            // Grammar:
+            // expression = term | expression `+` term | expression `-` term
+            // term = factor | term `*` factor | term `/` factor
+            // factor = `+` factor | `-` factor | `(` expression `)`
+            //        | number | functionName factor | factor `^` factor
+
+            double parseExpression() {
+                double x = parseTerm();
+                for (;;) {
+                    if      (eat('+')) x += parseTerm(); // addition
+                    else if (eat('-')) x -= parseTerm(); // subtraction
+                    else return x;
+                }
+            }
+
+            double parseTerm() {
+                double x = parseFactor();
+                for (;;) {
+                    if      (eat('*')) x *= parseFactor(); // multiplication
+                    else if (eat('/')) x /= parseFactor(); // division
+                    else return x;
+                }
+            }
+
+            double parseFactor() {
+                if (eat('+')) return parseFactor(); // unary plus
+                if (eat('-')) return -parseFactor(); // unary minus
+
+                double x;
+                int startPos = this.pos;
+                if (eat('(')) { // parentheses
+                    x = parseExpression();
+                    eat(')');
+                } else if ((ch >= '0' && ch <= '9') || ch == '.') { // numbers
+                    while ((ch >= '0' && ch <= '9') || ch == '.') nextChar();
+                    x = Double.parseDouble(str.substring(startPos, this.pos));
+                } else if (ch >= 'a' && ch <= 'z') { // functions
+                    while (ch >= 'a' && ch <= 'z') nextChar();
+                    String func = str.substring(startPos, this.pos);
+                    x = parseFactor();
+                    if (func.equals("sqrt")) x = Math.sqrt(x);
+                    else if (func.equals("sin")) x = Math.sin(Math.toRadians(x));
+                    else if (func.equals("cos")) x = Math.cos(Math.toRadians(x));
+                    else if (func.equals("tan")) x = Math.tan(Math.toRadians(x));
+                    else throw new RuntimeException("Unknown function: " + func);
+                } else {
+                    throw new RuntimeException("Unexpected: " + (char)ch);
+                }
+
+                if (eat('^')) x = Math.pow(x, parseFactor()); // exponentiation
+
+                return x;
+            }
+        }.parse();
     }
 
     private static boolean isNumeric(String str){
@@ -188,7 +274,7 @@ public class CALC2 {
         return true;
     }
 
-    private static void Print(String restOfCommand) {
+    private static void Print(String restOfCommand) { //prints a variable
         if(lstVars.containsKey(restOfCommand)){
             System.out.println("VALUE OF " + restOfCommand + " IS " + lstVars.get(restOfCommand));
         }
@@ -196,9 +282,9 @@ public class CALC2 {
 
     private static void Mem(String restOfCommand) {
         lstVars.put(restOfCommand, 0.0);
-    }
+    } //stores a variable
 
-    private static void Load(String command) {
+    private static void Load(String command) { //loads and stores a variable
         in = new Scanner(System.in);
         String key = command;
         Double value = 0.0;
